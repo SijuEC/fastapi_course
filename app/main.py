dffa2e51840072f1009cedfd51acea218ae22624
SijuEC <73427611+SijuEC@users.noreply.github.com>
2024-08-05 16:25:31 +0200

    adapted the code for docker deployment

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello world pushing out to docker"}
+    return {"message": "Hello beautiful world!!@@@!!!"}
